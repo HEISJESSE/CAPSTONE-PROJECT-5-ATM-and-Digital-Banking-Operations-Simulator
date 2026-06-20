e06dfd0d7faadf242234b5d6485d1ac26afe56a0
Muizzxviii <muizzmiftah01@gmail.com>
2026-06-20 16:27:27 +0100

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -1,1 +1,19 @@
-# To be implemented
+ class TransactionProcessor:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited: ₦{amount}")
+        print(f"New Balance: ₦{self.balance}")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrawn: ₦{amount}")
+            print(f"New Balance: ₦{self.balance}")
+        else:
+            print("Insufficient funds!")
+
+    def check_balance(self):
+        print(f"Current Balance: ₦{self.balance}")
