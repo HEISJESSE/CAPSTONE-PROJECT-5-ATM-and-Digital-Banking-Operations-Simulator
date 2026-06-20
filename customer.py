6ced5b1f13387c769d2792915298778eff7886f8
joshuambagwu934-cyber <joshuambagwu934@gmail.com>
2026-06-20 17:12:24 +0100

    Implement ATMCard class with PIN verification

--- customer.py
+++ customer.py
@@ -1,1 +1,23 @@
 # To be implemented
+class ATMCard:
+    def __init__(self, card_number, pin):
+        self.card_number = card_number
+        self.__pin = pin          # private attribute
+        self.blocked = False
+
+    def verify_pin(self, entered_pin):
+        if self.blocked:
+            return "Card is blocked"
+
+        if entered_pin == self.__pin:
+            return True
+        else:
+            return False
+
+    def block(self):
+        self.blocked = True
+        return "Card blocked successfully"
+
+    def unblock(self):
+        self.blocked = False
+        return "Card unblocked successfully"
