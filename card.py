2b6ef7d754fcd0f48f6473c52e6a7df4d55d2063
Mattsam <matthewsamuel918@gmail.com>
2026-06-20 17:12:41 +0100

    Update card.py

--- card.py
+++ card.py
@@ -1,1 +1,16 @@
-# To be implemented
+class ATMCard:
+    def __init__(self, card_number, pin):
+        self.card_number = card_number
+        self.pin = pin
+        self.blocked = False
+
+    def verify_pin(self, entered_pin):
+        if self.blocked:
+            return False
+        return self.pin == entered_pin
+
+    def block(self):
+        self.blocked = True
+
+    def unblock(self):
+        self.blocked = False
