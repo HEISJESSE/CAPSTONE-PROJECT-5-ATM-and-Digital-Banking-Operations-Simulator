c4db563c1cc8f9557957fffc5389e84d1d47d2c1
HEISJESSE <JESSEJEFFREY386@GMAIL.COM>
2026-06-20 16:02:50 +0100

    Add custom exception classes

--- exceptions.py
+++ exceptions.py
@@ -1,1 +1,14 @@
-# Custom exceptions
+class BankingError(Exception):
+    pass
+
+class InsufficientFundsError(BankingError):
+    pass
+
+class DailyLimitExceededError(BankingError):
+    pass
+
+class CardBlockedError(BankingError):
+    pass
+
+class FraudSuspicionError(BankingError):
+    pass
